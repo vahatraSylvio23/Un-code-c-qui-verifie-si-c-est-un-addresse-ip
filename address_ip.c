f8a836a519bf37c29e684238e4ec3604ee3e9291
vahatraSylvio23 <167562379+vahatraSylvio23@users.noreply.github.com>
2024-08-22 11:28:19 +0300

    Add files via upload
    L1 mit

---
+++ address_ip.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+typedef struct ip
+{
+    int a,b,c,d;
+}ip;
+void get_ip(char *argv ,ip *address);
+void objet();
+void verifie(ip address);
+void usage(int argc ,char *argv);
+int main(int argc ,char *argv[])
+{
+    char add_ip[100];
+    ip address;
+    usage(argc ,argv[0]);
+    objet();
+    get_ip(argv[1] ,&address);
+    verifie(address);
+    return 0;
+}
+void usage(int argc ,char *argv)
+{
+    if(argc==1)
+    {
+        fprintf(stderr,"Usage : %s xxx.xxx.xxx.xxx\n",argv);
+        exit(EXIT_SUCCESS);
+    }
+    if( (argv[4] != '.') && (argv[8] != '.') && (argv[12] != '.'))
+    {
+        printf("Ce n'est pas un addresse ip \n");
+        exit(EXIT_SUCCESS);
+    }
+}
+
+void objet()
+{
+    printf("Verifie si c'est un address ip\n");
+}
+
+void get_ip(char *argv ,ip *address)
+{
+    sscanf(argv,"%d.%d.%d.%d\n",&address->a,&address->b,&address->c,&address->d);
+    printf("%s\n",argv);
+    if(address->a > 255 || address->b > 255 || address->c > 255 || address->d > 255)
+    {
+        printf("Ce n'est pas un addresse ip\n");
+        exit(EXIT_SUCCESS);
+    }
+}
+void verifie(ip address)
+{
+    if(address.a >= 0 && address.a < 128)
+    {
+        printf("L'addresse ip est de class A \n");
+    }
+
+    if(address.a >= 128 && address.a < 192)
+    {
+        printf("L'addresse ip est de class B \n");
+    }
+
+    if(address.a >= 192 && address.a < 224)
+    {
+        printf("L'addresse ip est de class C \n");
+    }
+
+    if(address.a >= 224 && address.a < 240)
+    {
+        printf("L'addresse ip est de class D \n");
+    }
+
+    if(address.a >= 240 && address.a < 255)
+    {
+        printf("L'addresse ip est de class E \n");
+    }
+}
